6d860e63f666a6615a606b934edf0bd1da162d2e
Kuldeep Pisda <kuldeep.pisda@tejgyan.org>
2024-06-27 01:15:02 +0530

    Added user in the request

--- summarizer/requests/models/request.py
+++ summarizer/requests/models/request.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from utils.mixins.models.timestamp import TimeStampMixin
+
+User = get_user_model()
 
 
 class RequestStatus(models.TextChoices):
@@ -15,6 +18,12 @@ class Request(TimeStampMixin, models.Model):
     track = models.CharField(max_length=255, help_text="Name of the track.")
     song = models.ForeignKey(
         "Song", on_delete=models.SET_NULL, null=True, help_text="Reference to the song."
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text="Request made by the user.",
     )
     status = models.CharField(
         choices=RequestStatus.choices,
